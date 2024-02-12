e69dfd72b4296236b2fd2ef278758db103d9a7a9
start.vaadin.com <no-reply@vaadin.com>
2024-02-12 19:48:27 +0000

    Generated project

---
+++ src/main/java/com/ali/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.ali.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/ali/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.ali.services;
+
+import org.springframework.lang.NonNullApi;
